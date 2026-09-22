8a5ccc74fbee18e84a14e8c2277285377cc13cd7
hjunior29 <helderjuniorsilvalima@gmail.com>
2026-09-22 14:49:13 -0300

    docs(utils): improve function documentation

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -16211,3 +16211,21 @@ func ToKebabCase(s string) string {
 
 	return builder.String()
 }
+
+// DefaultIfEmpty returns the default value if the string is empty or contains only whitespace.
+//
+// @param s The input string to check.
+// @param defaultVal The default string value to return if `s` is empty or whitespace.
+// @return The input string `s` if it's not empty or whitespace, otherwise `defaultVal`.
+//
+// Examples:
+//
+//	DefaultIfEmpty("hello", "default") == "hello"
+//	DefaultIfEmpty("   ", "default") == "default"
+//	DefaultIfEmpty("", "default") == "default"
+func DefaultIfEmpty(s, defaultVal string) string {
+	if strings.TrimSpace(s) == "" {
+		return defaultVal
+	}
+	return s
+}
